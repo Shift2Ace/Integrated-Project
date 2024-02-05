32d7695b2b77e8236471a699948d15794004a932
Shift2Ace <hei20030719@gmail.com>
2024-02-05 13:44:07 +0800

    routes for driver done routes detail done

--- routes_detail.php
+++ routes_detail.php
@@ -1,3 +1,45 @@
+<?php
+    session_start();
+    $route_id = "";
+    if (isset($_GET["id"])){
+        $route_id_input = $_GET["id"];
+    } else {
+        header("Location: routes.php");
+    }
+
+
+    #connect
+    $conn = mysqli_connect("localhost", "default", "default", "webserver",3306);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "SELECT route_list.route_id, route_list.route_date, route_list.route_time, route_list.route_start, route_list.route_end, route_list.route_car, route_list.route_description, route_list.route_price,route_list.route_capacity, route_booking_count.booking_count, route_list.route_status, route_list.driver_id, route_list.driver_name, route_list.driver_email FROM route_list, route_booking_count WHERE route_list.route_id = route_booking_count.route_id AND route_list.route_id = '$route_id_input';";
+    if ($conn->multi_query($sql)){
+        $result = $conn->store_result();
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $route_id = $row["route_id"];
+                $route_date = $row["route_date"];
+                $route_time = $row["route_time"];
+                $route_start = $row["route_start"];
+                $route_end = $row["route_end"];
+                $booking_count = $row["booking_count"];
+                $route_capacity = $row["route_capacity"];
+                $route_price = $row["route_price"];
+                $route_description = $row["route_description"];
+                $driver_name = $row["driver_name"];
+                $driver_email = $row["driver_email"];
+                $route_car = $row["route_car"];
+                $driver_id = $row["driver_id"];
+            }
+            $result->free();
+        } else {
+            header("Location: routes.php");
+        }
+    }
+?>
+
 <html>
     <head>
         <link rel="stylesheet" href="main.css">
@@ -26,27 +68,32 @@
                             <tr>
                                 <td>Route ID</td>
                                 <td> : </td>
+                                <td><?php echo "$route_id"?></td>
                             </tr>
                             <tr>
                                 <td>Starting Location</td>
                                 <td> : </td>
-                                <td>  </td>
+                                <td><?php echo "$route_start"?></td>
                             </tr>
                             <tr>
                                 <td>Destination</td>
                                 <td> : </td>
+                                <td><?php echo "$route_end"?></td>
                             </tr>
                             <tr>
                                 <td>Date</td>
                                 <td> : </td>
+                                <td><?php echo "$route_date"?></td>
                             </tr>
                             <tr>
                                 <td>Time</td>
                                 <td> : </td>
+                                <td><?php echo "$route_time"?></td>
                             </tr>
                             <tr>
                                 <td>Price</td>
                                 <td> : </td>
+                                <td><?php echo "$route_price"?></td>
                             </tr>
                         </table>
                     </div>
@@ -56,14 +103,17 @@
                                 <tr>
                                     <td>Driver Name</td>
                                     <td> : </td>
+                                    <td><?php echo "$driver_name"?></td>
                                 </tr>
                                 <tr>
                                     <td>Driver Email</td>
                                     <td> : </td>
+                                    <td><?php echo "$driver_email"?></td>
                                 </tr>
                                 <tr>
                                     <td>Car model</td>
                                     <td> : </td>
+                                    <td><?php echo "$route_car"?></td>
                                 </tr>
                             </table>
                         </div>
@@ -72,7 +122,7 @@
                                 <tr>
                                     <td>Number of passenger</td>
                                     <td> : </td>
-                                    <td>10 / 150</td>
+                                    <td><?php echo "$booking_count / $route_capacity"?></td>
                                 </tr>
                                 
                             </table>
@@ -82,17 +132,18 @@
                 </div>
                 <div class="item" id="description">
                     Description <br><br>
-                    kasdjgflkjhbdklfjbsiub fiusdbdif start_districtsd
-                    sdf
-                    dfs adbfjhbdfjbsdjbf jdbfbdifbsidufb ukdbfsbdfbsd kjbfkhkhfkshfkhkfhjkjlfdjl
-                    sdf
-                    descriptionsdf
-                    sdff sdf sdf f sdf sdf ds sdaf sd sdf sd
+                    <?php echo nl2br($route_description)?>
                 </div>
                 <div id="button">
-                    <a href="" id="cancel" class="button bt_cancel">Cancel</a>
-                    <a href="" id="submit" class="button bt_apply">Book</a>
-                    <a href="" id="Delete" class="button bt_delete">Delete</a>
+                    <a id="cancel" href="routes.php" class="button bt_cancel">Cancel</a>
+                    <?php
+                        if ($_SESSION['login_status'] && $_SESSION['role']=="passenger"){
+                            echo "<a href='' id='submit' class='button bt_apply'>Book</a>";
+                        }
+                        if ($_SESSION['login_status'] && $_SESSION['role']=="driver" && $driver_id == $_SESSION["user_id"]){
+                            echo "<a href='' id='Delete' class='button bt_delete'>Delete</a>";
+                        }
+                    ?>
                 </div>
                 
             </div>
--- routes_dri.php
+++ routes_dri.php
@@ -1,21 +1,83 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['login_status'])){
+        header("Location: routes.php");
+    }else{
+        if (!$_SESSION['login_status']){
+            header("Location: routes.php");
+        }
+    }
+
+    if (!isset($_SESSION['role'])){
+        header("Location: routes.php");
+    } else {
+        if ($_SESSION['role'] != 'driver'){
+            header("Location: routes.php");
+        }
+    }
+
+    ini_set('display_errors',1);
+    error_reporting(-1);
+
+    $conn = mysqli_connect("localhost", "default", "default", "webserver",3306);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+?>
+
 <html>
     <head>
         <link rel="stylesheet" href="main.css">
         <style>
             .cl1 {
-                width: 50px;
+                width: 100px;
             }
             .cl2 {
-                width: 50%;
+                width: 130px;
             }
-            table {
-                background-color: #ddd;
+            .cl3 {
+                width: 100px;
             }
-            div.createRoute {
-                text-align: right;
-                padding: 2px;
-                margin-bottom: 10px;
+            .cl4 {
+                width: 300px;
             }
+            .cl5 {
+                width: 300px;
+            }
+            .cl6 {
+                width: auto;
+            }
+            .cl7 {
+                width: 120px;
+            }
+
+            .list {
+                width: 1500px;
+            }
+
+            .topHeader{
+                background-color: #004C99;
+                color: #FFF;
+                margin: 2px;
+                padding: 3px;
+                border-radius: 3px;
+                display: block;
+            }
+
+            .list .topHeader table{
+                width: 100%;
+                color: #FFF;
+            }
+
+            a.detail {
+                text-decoration: none;
+            }
+
+            div.item:hover {
+                background-color: #c8daeb;
+            }
+
         </style>
     </head>
     <body>
@@ -23,26 +85,61 @@
         <div class="container">
             <div class="list">
                 <div class="header">
-                    My Routes
+                    Routes
                 </div>
-                <div class="createRoute">
-                    <a href="routes_create.php" class="button bt_apply">Create New</a>
-                </div>
-                <div class="item">
+                <div class='topHeader'>
                     <table>
-                        <tr>
-                            <td class="cl1">
-                                1
-                            </td>
-                            <td class="cl2">
-                                2
-                            </td>
-                        </tr>
+                    <tr>
+                        <td class='cl1'>ID</td>
+                        <td class='cl2'>Date</td>
+                        <td class='cl3'>Time</td>
+                        <td class='cl4'>Start Location</td>
+                        <td class='cl5'>End Location</td>
+                        <td class='cl6'>Passenger</td>
+                        <td class='cl7'>Price (HKD)</td>
+                    </tr>
                     </table>
                 </div>
+                <?php
+                    $user_id = $_SESSION["user_id"];
+                    $sql = "SELECT route_list.route_id, route_list.route_date, route_list.route_time, route_list.route_start, route_list.route_end, route_list.route_price,route_list.route_capacity, route_booking_count.booking_count FROM route_list, route_booking_count WHERE route_list.route_id = route_booking_count.route_id AND route_list.driver_id = '$user_id'";
+                    if ($conn->multi_query($sql)){
+                        $result = $conn->store_result();
+                        while($row = $result->fetch_assoc()) {
+                            $route_id = $row["route_id"];
+                            $route_date = $row["route_date"];
+                            $route_time = $row["route_time"];
+                            $temp_start = $row["route_start"];
+                            $temp_end = $row["route_end"];
+                            $route_start = explode(",", $temp_start)[1];
+                            $route_end = explode(",", $temp_end)[1];
+                            $booking_count = $row["booking_count"];
+                            $route_capacity = $row["route_capacity"];
+                            $route_price = $row["route_price"];
+                            echo "<a href='routes_detail.php?id=$route_id' class='detail'>";
+                            echo "<div class='item'>";
+                            echo "<table>";
+                            echo "<tr>";
+                            echo "<td class='cl1'>"."$route_id"."</td>";
+                            echo "<td class='cl2'>"."$route_date"."</td>";
+                            echo "<td class='cl3'>"."$route_time"."</td>";
+                            echo "<td class='cl4'>"."$route_start"."</td>";
+                            echo "<td class='cl5'>"."$route_end"."</td>";
+                            echo "<td class='cl6'>"."$booking_count/$route_capacity"."</td>";
+                            echo "<td class='cl7'>"."$$route_price"."</td>";
+                            echo "</tr>";
+                            echo "</table>";
+                            echo "</div>";
+                            echo "</a>";
+                        }
+                        $result->free();
+                    }
+                    
+                    do {
+                        $conn->use_result();
+                    } while ($conn->more_results() && $conn->next_result());
+                ?>
             </div>
         </div>
-        
     </body>
-
 </html>
